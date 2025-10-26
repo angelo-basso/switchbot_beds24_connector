393d3c9f6d455fd737156de64430a4e58217e159
Angelo BASSO <angelo67170@gmail.com>
2025-10-26 20:42:44 +0100

    Add switchbot communication service

---
+++ src/services/switchbot.service.ts
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import { generateSwitchbotSign } from '../utils/hmac.util';
+import { logger } from '../utils/logger';
+
+const API_BASE = 'https://api.switch-bot.com/v1.1';
+
+export class SwitchBotService {
+    token: string;
+    secret: string;
+
+    constructor(token: string, secret: string) {
+        this.token = token;
+        this.secret = secret;
+    }
+
+    private headers() {
+        const nonce = uuidv4();
+        const t = Date.now().toString();
+        const sign = generateSwitchbotSign(this.token, this.secret, t, nonce);
+        return {
+            Authorization: this.token,
+            sign,
+            t,
+            nonce,
+            'Content-Type': 'application/json'
+        };
+    }
+
+    async createKeyAsync(deviceId: string, payload: any) {
+        const url = `${API_BASE}/devices/${deviceId}/commands`;
+        try {
+            const res = await axios.post(url, payload, { headers: this.headers(), timeout: 10000 });
+            return res.data;
+        } catch (err: any) {
+            logger.error({ err: err?.response?.data || err?.message }, 'SwitchBot createKey error');
+            throw err;
+        }
+    }
+
+    async deleteKeyAsync(deviceId: string, payload: any) {
+        const url = `${API_BASE}/devices/${deviceId}/commands`;
+        try {
+            const res = await axios.post(url, payload, { headers: this.headers(), timeout: 10000 });
+            return res.data;
+        } catch (err: any) {
+            logger.error({ err: err?.response?.data || err?.message }, 'SwitchBot deleteKey error');
+            throw err;
+        }
+    }
+}
